38062e4ecbfc65478f5a53cae0459e1461794c6f
AA\User <utt7418@gmail.com>
2025-06-01 20:13:19 +0900

    s

--- backend/models/badge.js
+++ backend/models/badge.js
@@ -1,32 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-  const Badge = sequelize.define('Badge', {
-    id: {
-      type: DataTypes.BIGINT, 
-      autoIncrement: true,
-      primaryKey: true,
-    },      
-    name: {
-      type: DataTypes.STRING(45),
-      allowNull: false,
+  const Badge = sequelize.define(
+    "Badge",
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
     },
-    img: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-  }, {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_general_ci',
-    timestamps: false,
-  });
+    {
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci",
+      timestamps: false,
+    }
+  );
 
   Badge.associate = (db) => {
     Badge.belongsToMany(db.User, {
-      through: 'user_badges', 
-      timestamps: true,       
+      through: "user_badges",
+      timestamps: true,
     });
   };
 
