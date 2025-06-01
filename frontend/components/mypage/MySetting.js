5b7a9c3ef31bdd4acd9df5e6769edf051ded9869
AA\User <utt7418@gmail.com>
2025-06-02 02:07:46 +0900

    UI add

--- frontend/components/mypage/MySetting.js
+++ frontend/components/mypage/MySetting.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Image } from "antd";
 
 const MySetting = () => {
-  return <div>dd</div>;
+  return (
+    <div style={{ backgroundColor: "lightgrey", minHeight: "100vh" }}>
+      <div></div>
+    </div>
+  );
 };
 
 export default MySetting;
--- frontend/pages/myset.js
+++ frontend/pages/myset.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
+import { Row, Col, Divider } from "antd";
+import AppLayout from "../components/AppLayout";
 import MySetting from "../components/mypage/MySetting";
+import MyAvatar from "../components/mypage/MyAvatar";
 
 const myset = () => {
-  return <></>;
+  return (
+    <AppLayout>
+      <Divider orientation="left">Percentage columns</Divider>
+      <Row>
+        <Col flex={2}>2 / 5</Col>
+        <Col flex={3}>3 / 5</Col>
+      </Row>
+    </AppLayout>
+  );
 };
 
 export default myset;
