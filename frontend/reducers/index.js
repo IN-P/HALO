23bf7911de47fcbb586c91890233f00f0736d6cd
yoon0416 <zgha16@gmail.com>
2025-06-05 18:17:03 +0900

    Update index.js

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -1,14 +1,11 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
-
 import post_IN from './post_IN';
 import user_YG from './user_YG';
-<<<<<<< HEAD
 import profile_jh from './profile_jh';
-=======
 import bookmark_IN from './bookmark_IN';
 import comment_IN from './comment_IN';
->>>>>>> d65074a5460ee92aada6e8c32f1c3216247301ac
+
 
 const rootReducer = combineReducers({
   index: (state = {}, action) => {
