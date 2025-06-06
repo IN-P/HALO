9ec1c005f3533c793a79627122b2b64d18232502
IN-P <cooladam_@naver.com>
2025-06-06 22:25:12 +0900

    error

--- frontend/sagas/index.js
+++ frontend/sagas/index.js
@@ -3,6 +3,9 @@ import post_IN from './post_IN'; //인
 import comment_IN from './comment_IN'; //인
 import user_YG from './user_YG'; //윤기
 import chatSaga from './chatSaga_JW'; //재원
+import followSaga from './follow_YB';//율비
+import reportSaga from './report_YB';
+
 import profile_jh from './profile_jh'; //준혁
 import bookmark_IN from './bookmark_IN'; //인
 
@@ -14,5 +17,7 @@ export default function* rootSaga() {
     fork(user_YG), //윤기
     fork(profile_jh), //준혁
     chatSaga(), //재원
+    followSaga(),//율비
+    fork(reportSaga),
   ]);
 }
