b11904228ea32d5ca447c2e94467f78eb77d7406
yulbi Jo <urby0725@naver.com>
2025-06-07 01:43:42 +0900

    .

--- frontend/sagas/block.js
+++ frontend/sagas/block.js
@@ -1,33 +1,54 @@
-// frontend/sagas/block.js
-
-import { all, fork, takeLatest, call, put } from 'redux-saga/effects';
+import { all, call, fork, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 import {
-  BLOCK_USER_REQUEST,
-  BLOCK_USER_SUCCESS,
-  BLOCK_USER_FAILURE,
+  BLOCK_USER_REQUEST, BLOCK_USER_SUCCESS, BLOCK_USER_FAILURE,
+  UNBLOCK_USER_REQUEST, UNBLOCK_USER_SUCCESS, UNBLOCK_USER_FAILURE,
+  LOAD_BLOCKS_REQUEST, LOAD_BLOCKS_SUCCESS, LOAD_BLOCKS_FAILURE,
 } from '../reducers/block';
 
-function blockUserAPI(toUserId) {
+function blockAPI(toUserId) {
   return axios.post('/block', { toUserId }, { withCredentials: true });
 }
 
 function* blockUser(action) {
   try {
-    yield call(blockUserAPI, action.data);
-    yield put({ type: BLOCK_USER_SUCCESS });
+    const result = yield call(blockAPI, action.data);
+    yield put({ type: BLOCK_USER_SUCCESS, data: result.data.Blocked || result.data });
   } catch (err) {
-    yield put({
-      type: BLOCK_USER_FAILURE,
-      error: err.response?.data || err.message,
-    });
+    yield put({ type: BLOCK_USER_FAILURE, error: err.response?.data || err.message });
   }
 }
 
-function* watchBlockUser() {
-  yield takeLatest(BLOCK_USER_REQUEST, blockUser);
+function unblockAPI(toUserId) {
+  return axios.delete(`/block/${toUserId}`, { withCredentials: true });
+}
+
+function* unblockUser(action) {
+  try {
+    yield call(unblockAPI, action.data);
+    yield put({ type: UNBLOCK_USER_SUCCESS, data: action.data });
+  } catch (err) {
+    yield put({ type: UNBLOCK_USER_FAILURE, error: err.response?.data || err.message });
+  }
+}
+
+function loadBlocksAPI() {
+  return axios.get('/block', { withCredentials: true });
+}
+
+function* loadBlocks() {
+  try {
+    const result = yield call(loadBlocksAPI);
+    yield put({ type: LOAD_BLOCKS_SUCCESS, data: result.data });
+  } catch (err) {
+    yield put({ type: LOAD_BLOCKS_FAILURE, error: err.response?.data || err.message });
+  }
 }
 
 export default function* blockSaga() {
-  yield all([fork(watchBlockUser)]);
+  yield all([
+    takeLatest(BLOCK_USER_REQUEST, blockUser),
+    takeLatest(UNBLOCK_USER_REQUEST, unblockUser),
+    takeLatest(LOAD_BLOCKS_REQUEST, loadBlocks),
+  ]);
 }
